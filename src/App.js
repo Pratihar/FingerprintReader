b69d495987eb7c1712419277641d4cf438dec9bc
Pratihar <dewanshu.pratihar@gamil.com>
2021-08-03 13:12:49 +0530

    Backend Shift

--- src/App.js
+++ src/App.js
@@ -155,7 +155,8 @@ function App() {
   // Example POST method implementation:
   async function postData(data = {}) {
     // Default options are marked with *
-    const response = await fetch("http://192.168.1.5:3000/bkyc", {
+    await fetch("https://api.kaabilfinance.com/bkyc", {
+      // await fetch("http://192.168.1.2:3000/bkyc", {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -167,9 +168,10 @@ function App() {
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    console.log(response.json());
-    return response.json(); // parses JSON response into native JavaScript objects
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err))
+      .then((data) => console.log(data)); // parses JSON response into native JavaScript objects
   }
 
   var postFingerprint = function (uid, url, callback) {
